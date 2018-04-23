46fa7510b41c6c74698e188d10ad6fa300e3bb50
Anusha Yalakala <anushayalakala@gmail.com>
2018-04-22 23:24:42 -0500

    refactor to clean up the code

--- reactfluxapp/src/Components/RepoInfo.js
+++ reactfluxapp/src/Components/RepoInfo.js
@@ -3,6 +3,7 @@ import { List, ListItem } from 'material-ui/List';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 import IconButton from 'material-ui/IconButton';
+import Divider from 'material-ui/Divider';
 import  '../Styles/RepoInfo.css';
 import RepositoryStore from '../Stores/RepositoryStore';
 import * as RepositoryActions from '../Actions/RepositoryActions';
@@ -17,7 +18,7 @@ export default class RepoInfo extends React.Component {
         this.styles = {
             div_style : {
                 paddingLeft: 50,
-                paddingRight: 50
+                paddingRight: 50,
             }, 
             h3_style: {
                 paddingLeft: 50,
@@ -34,9 +35,8 @@ export default class RepoInfo extends React.Component {
             },
             listitem_style: {
                 display: 'flex',
-                height: 80,
+                height: 'auto',
                 justifyContent: 'space-between',
-                border: '1px solid gray',
                 backgroundColor: 'white',
             },
             data_style : 
@@ -50,7 +50,7 @@ export default class RepoInfo extends React.Component {
                 alignItems: 'center',
                 padding: 10,
                 color:'blue'
-            }
+            },
         }
     }
     componentWillMount() {
@@ -85,13 +85,14 @@ export default class RepoInfo extends React.Component {
                                     const {committer} = commit;
                                     const commitdate = new Date(committer.date).toDateString();
                                     const hcode = (obj.sha).slice(0, 6);
-                                    return <div className= "listitem" style={this.styles.listitem_style}>
+                                    return <div><Divider/>
+                                    <div className= "listitem" style={this.styles.listitem_style}>
                                     <div  style= {this.styles.data_style}>
                                         <div style={this.styles.msg_style}>{commit.message}</div>
                                         <div style={this.styles.name_style}>{committer.name} commited on {commitdate}</div>
                                     </div>
                                         <div style={this.styles.hashcode_style}>{hcode}</div>
-                                    </div>
+                                    </div></div>
                                 })
                             }
                         </List>
---
+++ reactfluxapp/src/Styles/GistFiles.css.js
@@ -0,0 +1,17 @@
+export default {
+    name_style: {
+        paddingLeft: 30,
+        paddingRight: 30 
+      },
+      content_style: {
+        paddingLeft: 30,
+        paddingRight: 30
+      },
+      content:{
+        border:'1px solid red',
+        marginTop:10,
+        backgroundColor: 'white',
+        width:'80%',
+        margin:'0 auto'
+      }
+}
